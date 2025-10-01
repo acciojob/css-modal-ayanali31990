7fcfd93ef1bdfca85622dd46df8306129b977c0c
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-10-01 11:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+ const modal = document.getElementById("myModal");
+    const openBtn = document.getElementById("openModal");
+    const closeBtn = document.querySelector(".close-modal");
+
+    // Open modal
+    openBtn.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+
+    // Close modal on close button
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    // Close modal when clicking outside content
+    window.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });
